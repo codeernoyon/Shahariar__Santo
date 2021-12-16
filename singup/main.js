ca82ad1a9d3f1d5e644b2c0b520948275ed834d0
Nayan Islam <nayannk103@gmail.com>
2021-12-16 20:45:20 +0600

    full complite

---
+++ singup/main.js
@@ -0,0 +1,9 @@
+
+/**============================Nav Toggle==========================**/
+const navToggle = document.querySelector('.nev__toggole');
+const header = document.querySelector(".header");
+
+
+navToggle.addEventListener("click", function (){
+  header.classList.toggle('active');
+});
